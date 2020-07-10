3ee90c40fbee46bb3f7ad129dd8347a77a6ec0ec
Nathan Lafferty <nslafferty@gmail.com>
2020-07-10 16:33:13 -0500

    Setup

---
+++ MainMenu.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+
+func _on_Game_Start():
+	var res = get_tree().change_scene("res://SkyHigh.tscn")
+	if res != OK:
+		print("There was a massive failure")
+
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
---
+++ SkyHigh.gd
@@ -0,0 +1,11 @@
+extends Node
+
+export(int) var starting_height
+
+
+func _ready():
+	print("Game script ready")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
