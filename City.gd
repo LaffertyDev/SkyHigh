31d49621e46af5734b04101c289cea664c04b749
Nathan Lafferty <nslafferty@gmail.com>
2020-07-10 17:43:12 -0500

    Basic physics and lift model

--- City.gd
+++ City.gd
@@ -1,8 +1,17 @@
-extends RigidBody2D
+extends KinematicBody2D
 
 
 signal hit_ground
-signal city_move(height)
+signal city_move(height, velocity, accleration)
+
+export(Vector2) var velocity = Vector2(0, 0)
+
+var gravity_magnitude : int = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var balloons : int = 9
+var balloon_lift_newtons : int = 10 # divide by 10
+
+var current_mass_kg : int = 1
 
 func _ready():
 	pass # Replace with function body.
@@ -12,4 +21,15 @@ func _process(delta):
 	if (position > 0):
 		emit_signal("hit_ground")
 
-	emit_signal("city_move", position)
+	emit_signal("city_move", position, velocity.y, _get_accel())
+	
+	
+func _physics_process(delta):
+	velocity.y += _get_accel() * delta;
+	move_and_slide(velocity)
+
+func _get_accel():
+	var balloon_total_lift = (balloons * balloon_lift_newtons) / current_mass_kg
+	print(balloon_total_lift)
+	var acceleration_delta = float((gravity_magnitude - balloon_total_lift)) / 10.0 
+	return acceleration_delta
--- game_gui/GUI.gd
+++ game_gui/GUI.gd
@@ -1,8 +1,10 @@
 extends MarginContainer
 
 
-func _on_City_city_move(height):
-	var normalized_height = height * -1
+func _on_City_city_move(height, velocity, acceleration):
+	var normalized_height = floor(height * -1)
+	var normalized_velocity = floor(velocity * -1)
+	var normalized_acceleration = acceleration * -1
 	$HBoxContainer/Info/Height.text = "Height: " + str(normalized_height)
-	
-	pass # Replace with function body.
+	$HBoxContainer/Info/Velocity.text = "Velocity: " + str(normalized_velocity)
+	$HBoxContainer/Info/Acceleration.text = "Acceleration: " + str(normalized_acceleration)
