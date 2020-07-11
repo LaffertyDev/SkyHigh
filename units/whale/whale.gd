78fd09486ad18245458c6cea3a1f18b02d46ba77
Nathan Lafferty <lcgamer333@gmail.com>
2020-07-11 18:29:02 -0500

    Whales rotate

--- units/whale/whale.gd
+++ units/whale/whale.gd
@@ -1,11 +1,21 @@
-extends Area2D
+extends RigidBody2D
 
 export(int) var health = 1
+export(int) var min_speed = 10
+export(int) var max_speed = 100
 
 signal whale_died
 
 func _ready():
 	add_to_group("whales")
+	if randi() % 2 == 0:
+		print("go left")
+		$AnimatedSprite.flip_h = true
+		self.linear_velocity = Vector2(-rand_range(min_speed, max_speed), 0)
+	else:
+		print("go right")
+		self.linear_velocity = Vector2(rand_range(min_speed, max_speed), 0)
+
 
 func _on_Whale_input_event(viewport, event, shape_idx):
 	if event.is_action_pressed('click'):
